66364196d6916f6928f36320868fa41a99256d43
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-24 22:46:46 -0500

    commit tree e8bf8e6d12083acaecb78fce839cb113bedc9ce0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,7 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+    redirect_to student_path
   end
 
   def show
@@ -9,18 +10,19 @@ class StudentsController < ApplicationController
   end
 
   def new
-    
+   @student = Student.new 
   end
 
   def create
      new_student = Student.create(student_params)
-     redirect_to "/students/#{new_student.id}"
+    #  redirect_to "/students/#{new_student.id}"
+    redirect_to student_path(new_student)
   end
 
   private
 
   def student_params
-    params.require(:student).permit(:first_name)
+    params.require(:student).permit(:first_name, :last_name)
   end
 
 end
